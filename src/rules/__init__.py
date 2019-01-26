16bfcb0f92f398782474c8ebb2de9bd3512a7e96
Daniel Parker <danpker@gmail.com>
2019-01-26 19:09:20 +0000

    Restructure code

---
+++ src/rules/__init__.py
@@ -0,0 +1,13 @@
+"""Rules module for handling and validating rules."""
+
+from rules.rules import (
+    any_match,
+    illegal_words,
+    number_of_words,
+)
+
+__all__ = [
+    "any_match",
+    "illegal_words",
+    "number_of_words",
+]
