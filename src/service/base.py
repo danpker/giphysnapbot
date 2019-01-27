ce36c5f343d60aa11c49ee2065ce1699f0a0a682
Daniel Parker <danpker@gmail.com>
2019-01-27 07:42:58 +0000

    Fix lint

--- src/service/base.py
+++ src/service/base.py
@@ -6,10 +6,12 @@ class GiphySnapBotBase():
     """Base class for GiphySnapBot to hide connecting stuff."""
 
     def __init__(self, config):
+        """Create a GiphySnapBotBase instance and connect to Slack."""
         self._slack_client = self._connect(config["slack_bot_token"])
         self._bot_id = self._get_user_id()
 
     def _connect(self, slack_bot_token):
+        """Connect to Slack and connect to the RTM API."""
         slack_client = SlackClient(slack_bot_token)
         connect_response = slack_client.rtm_connect(
             with_team_state=False, auto_reconnect=True)
@@ -20,6 +22,7 @@ class GiphySnapBotBase():
             raise ConnectionError(connect_response["error"])
 
     def _get_user_id(self):
+        """Get the bot's user id from Slack."""
         auth_response = self._slack_client.api_call("auth.test")
 
         if auth_response.get("ok") is not None and auth_response["ok"]:
